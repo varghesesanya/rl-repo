446e4177bcce75bb8708e9ded7ae438822de23fd
“Sanya <varghese.sanya@gmail.com>
2024-11-25 17:14:08 -0500

    Added Inference on the new multigate

--- scripts/inferencing_learning_new_gate_on_multi_gate.py
+++ scripts/inferencing_learning_new_gate_on_multi_gate.py
@@ -6,6 +6,7 @@ from relaqs.environments.single_qubit_env import SingleQubitEnv
 from relaqs.plot_data import plot_data, plot_training_and_inferencing
 from relaqs.save_results import SaveResults
 from relaqs.api.utils import do_inferencing_new_gate, run_multigate_training, tic, toc
+import pandas as pd
 
 
 training_gates = [gates.X(), gates.Y(), gates.Z()]
@@ -37,9 +38,6 @@ trained_alg, results = run_multigate_training(env_class=GateSynthEnvRLlibHaarNoi
         n_training_iterations=n_training_iterations,
         noise_file=noise_file, path_to_save_checkpoints=path_to_save_checkpoints
 )
-print("Trained Algorithm:", trained_alg, results)
-#print("Results:", results)
-
 # -------------------> Save Training Results <---------------------------------------
 
 if save_training is True:
@@ -57,46 +55,46 @@ if save_training is True:
                 print("Training plots created.")
 
 
-# # ------------------> Inferencing on New Gate <------------------------
-# # Configure the new target gate for inferencing
-# new_target_gate = gates.Y() 
-# inferenced_env = do_inferencing_new_gate(env,
-#     trained_alg,
-#     n_episodes_for_inferencing,
-#     quantum_noise_file_path=inferencing_noise_file,
-#     new_gate=new_target_gate
-# )
+# ------------------> Inferencing on New Gate <------------------------
+# Configure the new target gate for inferencing
+new_target_gate = gates.Y() 
+inferenced_env = do_inferencing_new_gate(env,
+    trained_alg,
+    n_episodes_for_inferencing,
+    quantum_noise_file_path=inferencing_noise_file,
+    new_gate=new_target_gate
+)
 
 
 
-# # ------------------> Save Inferencing Results <------------------------
-# if save_inference:
-#     sr = SaveResults(inferenced_env, trained_alg)
-#     save_dir_inference = sr.save_results("inference")
-#     print("Inferencing results saved to:", save_dir_inference)
+# ------------------> Save Inferencing Results <------------------------
+if save_inference:
+    sr = SaveResults(inferenced_env, trained_alg)
+    save_dir_inference = sr.save_results("inference")
+    print("Inferencing results saved to:", save_dir_inference)
     
-#     if plot:
-#         plot_data(
-#         save_dir_inference,
-#         episode_length=trained_alg._episode_history[0].episode_length,
-#         figure_title=figure_title_inference,
-#         env_data_path="env_data_inference.csv"
-#         )
-#         print("Inference plots created.")
+    if plot:
+        plot_data(
+        save_dir_inference,
+        episode_length=trained_alg._episode_history[0].episode_length,
+        figure_title=figure_title_inference,
+        env_data_path="env_data_inference.csv"
+        )
+        print("Inference plots created.")
 
-# # Define the gate names and save path
-# training_gate_name = "X, Y, Z and H Gates"
-# inference_gate_name = "Y Gate"
-# plot_save_path = "./training_vs_inference_fidelity_with_gate_names.png"
+# Define the gate names and save path
+training_gate_name = "X, Y, Z and H Gates"
+inference_gate_name = "Y Gate"
+plot_save_path = "./training_vs_inference_fidelity_with_gate_names.png"
 
-# # Plot training vs inference and save the plot
-# plot_training_and_inferencing(
-#     training_save_dir=save_dir_training,       
-#     inference_save_dir=save_dir_inference,  
-#     training_gate_name=training_gate_name,  
-#     inference_gate_name=inference_gate_name, 
-#     figure_title="Training vs Inference Fidelity",
-#     save_path=plot_save_path            # Path to save the plot
-# )
+# Plot training vs inference and save the plot
+plot_training_and_inferencing(
+    training_save_dir=save_dir_training,       
+    inference_save_dir=save_dir_inference,  
+    training_gate_name=training_gate_name,  
+    inference_gate_name=inference_gate_name, 
+    figure_title="Training vs Inference Fidelity",
+    save_path=plot_save_path            # Path to save the plot
+)
 
-# print(f"Elapsed Time: {toc(start):.4f} seconds")
+print(f"Elapsed Time: {toc(start):.4f} seconds")
